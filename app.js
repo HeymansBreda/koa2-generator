83f68929a1f653536467ce328b9bc7704a98fbeb
wa1988 <wa1321042753@live.com>
2017-07-20 08:41:29 +0800

    static file

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const Koa = require('koa');
 
 const app = new Koa();
 
+const process = require('./process');
+const templating = require('./templating');
 const controller = require('./controller');
 
+const isProduction = process.env.NODE_ENV === 'production';
 
+
+app.use(templating('view', {
+  noCache: !isProduction,
+  watch: !isProduction
+}));
+app.use(process());
 // Router
 app.use(controller());
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,29 @@
 const fn_index = async (ctx, next) => {
-  ctx.body = `<h1>Hello, world</h1>`
+  ctx.render('index.html', {
+    title: 'Welcome'
+  })
 };
 
+const fn_signin = async (ctx, next) => {
+  var
+    email = ctx.request.body.email || '',
+    password = ctx.request.body.password || '';
+
+  if (email == 'mail@email.com' && password === 'password') {
+    // success
+    ctx.render('signin-ok.html', {
+      title: 'Sign In OK',
+      name: 'Mr Node'
+    })
+  } else {
+    // error
+    ctx.render('signin-failed.html', {
+      title: 'Sign In Failed'
+    })
+  }
+};
 
 module.exports = {
   'GET /': fn_index,
+  'POST /signin': fn_signin
 };
